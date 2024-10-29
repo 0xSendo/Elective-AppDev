0a1857bf118741150f580eef111d57b8a46fb423
0xSendo <142055962+0xSendo@users.noreply.github.com>
2024-10-29 16:58:01 +0800

    Update App.jsx

--- IntelliHealth/src/App.jsx
+++ IntelliHealth/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './assets/Components/Login'; 
+import Login from './assets/Components/Login';
 import SignUp from './assets/Components/SignUp';
+import HomePage from './assets/Components/HomePage';
+import ProfileInput from './assets/Components/ProfileInput';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/profile" element={<ProfileInput />} />
+      <Route path="/home" element={<HomePage />} /> {/* Updated to /home for consistency */}
     </Routes>
   </Router>
 );
