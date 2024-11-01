f895c5c097655674105685e9f04ecd25c35897cc
0xSendo <142055962+0xSendo@users.noreply.github.com>
2024-11-01 22:57:28 +0800

    Update App.jsx

--- IntelliHealth/src/App.jsx
+++ IntelliHealth/src/App.jsx
@@ -4,14 +4,18 @@ import Login from './assets/Components/Login';
 import SignUp from './assets/Components/SignUp';
 import HomePage from './assets/Components/HomePage';
 import ProfileInput from './assets/Components/ProfileInput';
+import LandingPage from './assets/Components/LandingPage';
+import HomePage2 from './assets/Components/HomePage2'; // Import HomePage2
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/profile" element={<ProfileInput />} />
-      <Route path="/home" element={<HomePage />} /> {/* Updated to /home for consistency */}
+      <Route path="/home" element={<HomePage />} /> {/* Change here for consistency */}
+      <Route path="/home2" element={<HomePage2 />} /> {/* Added route for HomePage2 */}
     </Routes>
   </Router>
 );
