762c7422b72f706d2175e1865dd241357aaf096a
doraratt <166276505+doraratt@users.noreply.github.com>
2024-11-06 12:51:24 +0800

    Update Login.jsx

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';  
+import axios from 'axios';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    try {
+        const userData = {
+            email: email, // assuming you're capturing this in state
+            password: password, // assuming you're capturing this in state
+        };
 
-    
-    if (email === 'test@test.com' && password === 'password') {
-      navigate('/home'); 
-    } else {
-      alert('Invalid credentials');
+        // Make the POST request to the API
+        const response = await axios.post('http://localhost:8080/api/users/login', userData);
+        console.log("Login successful:", response.data);
+        localStorage.setItem('user', JSON.stringify(response.data));
+        navigate('/profile');            // Redirect or handle successful login here
+    } catch (error) {
+        if (error.response) {
+            alert("Login failed. Invalid credentials");
+            console.error(error.response.data);
+        } else if (error.request) {
+            console.error("Login failed. No response received from the server:", error.request);
+        } else {
+            console.error("Login failed. Error:", error.message);
+        }
     }
-  };
+};
 
   return (
     <div className="form-container">
@@ -31,6 +45,7 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
             required
           />
+          
         </div>
         <div className="input-group">
           <input
