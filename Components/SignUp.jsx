b2e417784d3995061cdbf27e05fefde5c62b8cad
ToniRubs14 <166927721+ToniRubs14@users.noreply.github.com>
2024-11-19 01:00:39 +0800

    Update SignUp.jsx

--- Components/SignUp.jsx
+++ Components/SignUp.jsx
@@ -1,39 +1,37 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Login.css';  
-
+import './Login.css';
 import axios from 'axios';
 
 const SignUp = () => {
   const [signUpData, setSignUpData] = useState({ name: '', email: '', password: '' });
   const navigate = useNavigate();
-  const [error, setError] = useState('')
+  const [error, setError] = useState('');
 
   const handleSignUpChange = (e) => {
     const { name, value } = e.target;
-      setSignUpData({ ...signUpData, [name]: value });
-  
+    setSignUpData({ ...signUpData, [name]: value });
   };
 
-  const handleSignUp = async (e) =>  {
+  const handleSignUp = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post(`http://localhost:8080/api/users/signUp`, signUpData);
       console.log("Sign-Up successful:", response.data);
-      console.log("this is response data status",response.data.status)
-      if(response.status === 201){
-        navigate('/profile'); 
+      console.log("This is response data status:", response.data.status);
+      if (response.status === 201) {
+        navigate('/login'); // Redirect to login page instead of profile
         setError('');
       }
     } catch (error) {
-      if (error.response.status === 400) {
+      if (error.response && error.response.status === 400) {
         setError('Email already in use. Please try again.');
       } else {
         setError('Sign Up failed. Please try again.');
       }
       console.error('Sign-Up error:', error);
-    } 
+    }
   };
 
   return (
@@ -44,7 +42,7 @@ const SignUp = () => {
           <input
             type="text"
             placeholder="Name"
-            name='name'
+            name="name"
             value={signUpData.name}
             onChange={handleSignUpChange}
             required
@@ -54,7 +52,7 @@ const SignUp = () => {
           <input
             type="email"
             placeholder="Email"
-            name='email'
+            name="email"
             value={signUpData.email}
             onChange={handleSignUpChange}
             required
@@ -64,14 +62,14 @@ const SignUp = () => {
           <input
             type="password"
             placeholder="Password"
-            name='password'
+            name="password"
             value={signUpData.password}
             onChange={handleSignUpChange}
             required
           />
         </div>
         <button type="submit">Sign Up</button>
-        <p style={{color:'red', marginTop:'10px', display:'flex', justifyContent:'center'}}>
+        <p style={{ color: 'red', marginTop: '10px', display: 'flex', justifyContent: 'center' }}>
           {error}
         </p>
         <p className="signup-link">
