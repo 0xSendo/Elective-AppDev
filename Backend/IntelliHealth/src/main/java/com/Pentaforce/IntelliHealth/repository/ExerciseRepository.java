9099dc15ec3d9b333bd5ddb073b01c9682d296ee
doraratt <166276505+doraratt@users.noreply.github.com>
2024-12-02 01:16:58 +0800

    Update ExerciseRepository.java

--- Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/repository/ExerciseRepository.java
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/repository/ExerciseRepository.java
@@ -1,9 +1,10 @@
 package com.Pentaforce.IntelliHealth.repository;
 
-import com.Pentaforce.IntelliHealth.entity.ExerciseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.Pentaforce.IntelliHealth.entity.ExerciseEntity;
+
 @Repository
-public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Long> {
+public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Integer> {
 }
