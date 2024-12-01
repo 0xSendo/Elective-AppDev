9dcbb83ae4fd1bb37ea651c3cbd6849e0b5e91a8
Angelo_Elca <169126519+TiredShaman@users.noreply.github.com>
2024-12-02 01:13:04 +0800

    Create GoalNotFoundException.java
    added to go work with goal

---
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/exception/GoalNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Pentaforce.IntelliHealth.exception;
+
+public class GoalNotFoundException extends RuntimeException {
+    public GoalNotFoundException(String message) {
+        super(message);
+    }
+}
