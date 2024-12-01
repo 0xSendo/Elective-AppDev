cd2afe6592455d730e9ecb4919bcd87f422b5061
doraratt <166276505+doraratt@users.noreply.github.com>
2024-12-02 01:15:45 +0800

    Update GoalEntity.java

--- Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/entity/GoalEntity.java
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/entity/GoalEntity.java
@@ -1,7 +1,7 @@
 package com.Pentaforce.IntelliHealth.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tblgoal")
@@ -11,27 +11,21 @@ public class GoalEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int goalID;
 
-    @ManyToOne
-    @JoinColumn(name = "userID", nullable = false)
-    private UserEntity user;
-
-    private String type;
-
+    // @ManyToOne
+    // @JoinColumn(name = "userID", nullable = false)
+    // private UserEntity user;
     private String target;
-
     private LocalDate deadline;
 
-    private float progress;
-
     // Constructors
-    public GoalEntity() {}
+    public GoalEntity() {
+        
+    }
 
-    public GoalEntity(UserEntity user, String type, String target, LocalDate deadline, float progress) {
-        this.user = user;
-        this.type = type;
+    public GoalEntity(String target, LocalDate deadline) {
+        //this.user = user;
         this.target = target;
         this.deadline = deadline;
-        this.progress = progress;
     }
 
     // Getters and Setters
@@ -43,21 +37,13 @@ public class GoalEntity {
         this.goalID = goalID;
     }
 
-    public UserEntity getUser() {
-        return user;
-    }
+    // public UserEntity getUser() {
+    //     return user;
+    // }
 
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+    // public void setUser(UserEntity user) {
+    //     this.user = user;
+    // }
 
     public String getTarget() {
         return target;
@@ -73,13 +59,5 @@ public class GoalEntity {
 
     public void setDeadline(LocalDate deadline) {
         this.deadline = deadline;
-    }
-
-    public float getProgress() {
-        return progress;
-    }
-
-    public void setProgress(float progress) {
-        this.progress = progress;
     }
 }
