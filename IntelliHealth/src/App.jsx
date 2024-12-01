d3173fed2e37bd0c42559b614617a47d3015b2d8
Angelo_Elca <169126519+TiredShaman@users.noreply.github.com>
2024-12-02 01:14:23 +0800

    Update App.jsx
    for routing of other jsx file

--- IntelliHealth/src/App.jsx
+++ IntelliHealth/src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Component Imports
 import Login from './assets/Components/Login';
 import SignUp from './assets/Components/SignUp';
-import HomePage from './assets/Components/HomePage';
+//import HomePage from './assets/Components/HomePage';
 import ProfileInput from './assets/Components/ProfileInput';
 import LandingPage from './assets/Components/LandingPage';
 import HomePage2 from './assets/Components/HomePage2';
 import HomePage3 from './assets/Components/HomePage3';
 import HomeSidebar from './assets/Components/HomeSidebar';
 import Settings from './assets/Components/Settings';
+import Home from './assets/Components/Home';
+import Login2 from './assets/Components/Login2';
 
 // Exercise Component Imports
 import Cardio from './assets/Exercise/Cardio';
@@ -18,13 +20,12 @@ import Strength from './assets/Exercise/Strength';
 import Balance from './assets/Exercise/Balance';
 import Flexibility from './assets/Exercise/Flexibility';
 import Core from './assets/Exercise/Core';
+import Endurance from './assets/Exercise/Endurance';
 
 // Feature Component Imports
 import Goal from './assets/Entity/Goal';
 import Progress from './assets/Entity/Progress';
 import WorkoutPlan from './assets/Entity/WorkoutPlan';
-import Recent from './assets/Entity/Recent';
-// import Recent from './assets/Entity/Recent';
 
 const App = () => (
   <Router>
@@ -34,24 +35,26 @@ const App = () => (
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/profile" element={<ProfileInput />} />
-      <Route path="/home" element={<HomePage />} />
+      {/* <Route path="/home" element={<HomePage />} /> */}
       <Route path="/home2" element={<HomePage2 />} />
-      <Route path="/home3" element={<HomePage3 />} />
+      <Route path="/home" element={<HomePage3 />} />
       <Route path="/hs" element={<HomeSidebar />} />
       <Route path="/settings" element={<Settings />} />
-
+      <Route path="/hm" element={<Home />} />
+      <Route path="/login2" element={<Login2 />} />
+      
       {/* Exercise Pages */}
       <Route path="cardio" element={<Cardio />} />
       <Route path="strength" element={<Strength />} />
       <Route path="balance" element={<Balance />} />
       <Route path="flexibility" element={<Flexibility />} />
       <Route path="core" element={<Core />} />
+      <Route path="endurance" element={<Endurance />} />
 
       {/* Feature Pages */}
       <Route path="/goal" element={<Goal />} />
       <Route path="/progress" element={<Progress />} />
       <Route path="/wp" element={<WorkoutPlan />} />
-      <Route path="/recent" element={<Recent/>}/>
     </Routes>
   </Router>
 );
