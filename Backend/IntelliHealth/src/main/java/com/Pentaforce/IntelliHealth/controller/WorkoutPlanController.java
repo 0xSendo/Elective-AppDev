9b8162430a222eb785e4e0160befe67da0c08f4b
Angelo_Elca <169126519+TiredShaman@users.noreply.github.com>
2024-12-02 01:05:25 +0800

    Update WorkoutPlanController.java
    added new attribute and remove some redundant attributes

--- Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/controller/WorkoutPlanController.java
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/controller/WorkoutPlanController.java
@@ -6,57 +6,43 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.Pentaforce.IntelliHealth.entity.WorkoutPlan;
+import com.Pentaforce.IntelliHealth.entity.WorkoutPlanEntity;
 import com.Pentaforce.IntelliHealth.service.WorkoutPlanService;
 
 @RestController
 @RequestMapping("/api/workoutplans")
+@CrossOrigin(origins = "http://localhost:5173")
 public class WorkoutPlanController {
 
     @Autowired
     private WorkoutPlanService workoutPlanService;
-
-    // Create a new workout plan
+   
     @PostMapping
-    public ResponseEntity<WorkoutPlan> createWorkoutPlan(@RequestBody WorkoutPlan workoutPlan) {
-        WorkoutPlan createdWorkoutPlan = workoutPlanService.createWorkoutPlan(workoutPlan);
-        return new ResponseEntity<>(createdWorkoutPlan, HttpStatus.CREATED);
+    public ResponseEntity<WorkoutPlanEntity> createWorkoutPlan(@RequestBody WorkoutPlanEntity workoutPlan) {
+        WorkoutPlanEntity createdPlan = workoutPlanService.createWorkoutPlan(workoutPlan);
+        return new ResponseEntity<>(createdPlan, HttpStatus.CREATED);
     }
 
-    // Get all workout plans
     @GetMapping
-    public List<WorkoutPlan> getAllWorkoutPlans() {
-        return workoutPlanService.getAllWorkoutPlans();
+    public ResponseEntity<List<WorkoutPlanEntity>> getAllWorkoutPlans() {
+        List<WorkoutPlanEntity> workoutPlans = workoutPlanService.getAllWorkoutPlans();
+        return new ResponseEntity<>(workoutPlans, HttpStatus.OK);
     }
 
-    // Get a workout plan by ID
-    @GetMapping("/{planID}")
-    public ResponseEntity<WorkoutPlan> getWorkoutPlanById(@PathVariable Integer planID) {
-        Optional<WorkoutPlan> workoutPlan = workoutPlanService.getWorkoutPlanById(planID);
-        return workoutPlan.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                          .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    // Update a workout plan
     @PutMapping("/{planID}")
-    public ResponseEntity<WorkoutPlan> updateWorkoutPlan(@PathVariable Integer planID, @RequestBody WorkoutPlan workoutPlanDetails) {
-        WorkoutPlan updatedWorkoutPlan = workoutPlanService.updateWorkoutPlan(planID, workoutPlanDetails);
-        return updatedWorkoutPlan != null ? new ResponseEntity<>(updatedWorkoutPlan, HttpStatus.OK)
-                                          : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<WorkoutPlanEntity> updateWorkoutPlan(@PathVariable Integer planID, @RequestBody WorkoutPlanEntity workoutPlan) {
+        Optional<WorkoutPlanEntity> existingPlan = workoutPlanService.getWorkoutPlanById(planID);
+        if (existingPlan.isPresent()) {
+            WorkoutPlanEntity updatedPlan = workoutPlanService.updateWorkoutPlan(planID, workoutPlan);
+            return new ResponseEntity<>(updatedPlan, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // Delete a workout plan
     @DeleteMapping("/{planID}")
-    public ResponseEntity<HttpStatus> deleteWorkoutPlan(@PathVariable Integer planID) {
+    public ResponseEntity<Void> deleteWorkoutPlan(@PathVariable Integer planID) {
         workoutPlanService.deleteWorkoutPlan(planID);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
