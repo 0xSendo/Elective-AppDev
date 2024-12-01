d519da01265a31c49c24f7648849f43b03c4f0ad
Angelo_Elca <169126519+TiredShaman@users.noreply.github.com>
2024-12-02 01:02:52 +0800

    Update UserEntity.java
    added new attribute and remove some redundant attributes

--- Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/entity/UserEntity.java
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/entity/UserEntity.java
@@ -1,6 +1,5 @@
 package com.Pentaforce.IntelliHealth.entity;
 
-//import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,52 +7,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tbluser")
+@Table(name = "tbluser")
 public class UserEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userID;
 
-    //@Column(nullable = false)                 //cannot be empty
     private String name;
-
-    //@Column(nullable = false, unique=true)      //cannot be empty, no duplication allowed
     private String email;
-
-    //@Column(nullable = false)
     private String password;
 
-    //@Column(nullable = false)                 //cant successfully sign up if this line is coded
-    private String fitnessLevel;
+    // Constructors
+    public UserEntity() {}
 
-    //@Column(nullable = false)
-    private String gender;
-
-    //@Column(nullable = false)
-    private float weight;
-
-    //@Column(nullable = false)
-    private float height;
-
-    //@Column(nullable = false)
-    private float weightGoal;
-
-    
-    //Constructors
-    public UserEntity(){
-
-    }
-
-    public UserEntity(String name, String email, String password, String gender, String fitnessLevel, float weight, float height, float weightGoal) {
+    public UserEntity(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.gender = gender;
-        this.fitnessLevel = fitnessLevel;
-        this.weight = weight;
-        this.height = height;
-        this.weightGoal = weightGoal;
     }
 
     // Getters and setters
@@ -61,7 +32,7 @@ public class UserEntity {
         return userID;
     }
 
-    public void setUserID(int userID) {
+    public void setUserId(int userID) {
         this.userID = userID;
     }
 
@@ -88,48 +59,4 @@ public class UserEntity {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getFitnessLevel() {
-        return fitnessLevel;
-    }
-
-    public void setFitnessLevel(String fitnessLevel) {
-        this.fitnessLevel = fitnessLevel;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-
-    }
-
-    public float getWeightGoal() {
-        return weightGoal;
-    }
-
-    public void setWeightGoal(float weightGoal) {
-        this.weightGoal = weightGoal;
-    }
-
-   
-
 }
