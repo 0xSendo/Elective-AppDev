7d14f66a77aa700ed65c56c26b1fe992aa2b40ec
Angelo_Elca <169126519+TiredShaman@users.noreply.github.com>
2024-12-02 01:07:30 +0800

    Update UserRepository.java

--- Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/repository/UserRepository.java
+++ Backend/IntelliHealth/src/main/java/com/Pentaforce/IntelliHealth/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.Pentaforce.IntelliHealth.repository;
 
-import com.Pentaforce.IntelliHealth.entity.UserEntity;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.Pentaforce.IntelliHealth.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByEmail(String email);
 }
-
-
