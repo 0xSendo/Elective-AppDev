65cfcabc150b3b07f54cf91ffd018fabd9703160
kylesumucad <kaelrubio1018@gmail.com>
2024-12-02 13:47:32 +0800

    Update Core

--- Exercise/Core.jsx
+++ Exercise/Core.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+
+//muis
 import {
   Box,
   Button,
@@ -15,6 +16,9 @@ import {
   Snackbar,
   Alert
 } from '@mui/material';
+
+//connection to backend
+import axios from 'axios';
 
 const Core = () => {
   const navigate = useNavigate();
